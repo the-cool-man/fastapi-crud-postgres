6b8605ca926a640e71c88f293bf215055af65b0e
Tohid Kovadiya <kovadiya.tohid@gmail.com>
2024-12-22 22:13:37 +0530

    Delete and update endpoint added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel  # Used for validation
 from random import randrange
 
@@ -19,10 +19,19 @@ my_post = [
     {'id': 3, 'title': 'Food', 'Content': "Dal Fry and Roti"},
 ]
 
+
 def findPostByID(id):
     for p in my_post:
         if p['id'] == id:
             return p
+
+
+def findIndex(id):
+    for i, p in enumerate(my_post):
+        if p['id'] == id:
+            return i
+        
+
 
 @app.get("/")
 def root():
@@ -34,7 +43,7 @@ def get_posts():
     return { 'data': my_post}
 
 
-@app.post("/post")
+@app.post("/post", status_code=status.HTTP_201_CREATED)
 def create_post(payload: Post):
     post_dic = payload.model_dump()
     post_dic["id"] = randrange(0, 100)
@@ -44,4 +53,30 @@ def create_post(payload: Post):
 
 @app.get("/post/{id}")
 def get_post(id: int):
-    return { 'data': findPostByID(id)}
+    post = findPostByID(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No post with id {id} is available")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return { 'message': 'No post available'}
+    return { 'data': post}
+
+
+@app.delete("/post/{id}" ,status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    postIndex = findIndex(id)
+    if postIndex != None:
+        my_post.pop(postIndex)
+    return { 'data': my_post}
+
+
+@app.put("/post/{id}")
+def update_post(id: int, post: Post):
+    postIndex = findIndex(id)
+    if postIndex == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No post with id {id} is available to update")
+    
+    post_dic = post.model_dump()
+    post_dic['id'] = id
+    my_post[postIndex] = post_dic
+    return {'data': post_dic}
+    
